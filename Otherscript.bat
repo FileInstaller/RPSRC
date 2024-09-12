e6cf6f8596a72e539599d18ce56cdadaa7e795bc
queviding <rubansasa262@gmail.com>
2024-09-12 12:59:01 +0900

    Update Otherscript.bat

--- Otherscript.bat
+++ Otherscript.bat
@@ -1,3 +1,25 @@
 @echo off
+setlocal enabledelayedexpansion
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+REM              SETUP
+TITLE Roblox Account Changer
+Color 0f
+mode con cols=89 lines=29
+for /F %%a in ('echo prompt $E ^| cmd') do set "ESC=%%a"
+chcp 65001 > nul
+cls
+
+REM               LOADLOGO
+:LOADLOGO
 echo]
-echo this file be a receicver to other file and updater for this bat file.
+echo]
+echo %ESC%[95m    ██████╗  ██████╗     ██████╗ ██████╗
+echo     ██╔══██╗██╔════╝    ██╔════╝██╔════╝
+echo     ██║  ██║██║  ███╗   ██║     ██║     
+echo     ██║  ██║██║   ██║   ██║     ██║     
+echo     ██████╔╝╚██████╔╝██╗╚██████╗╚██████╗
+echo     ╚═════╝  ╚═════╝ ╚═╝ ╚═════╝ ╚═════╝
+echo                     Python installer
+ping 192.0.2.2 -n 1 -w 1000 > nul
+echo]
+echo %ESC%[31mFile has no updates, %ESC%[32mlast version downloaded!
