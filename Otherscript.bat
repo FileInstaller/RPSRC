53278c296a60da5c05c7e2eb49c266f8a45adf15
queviding <rubansasa262@gmail.com>
2024-09-12 13:01:59 +0900

    Update Otherscript.bat

--- Otherscript.bat
+++ Otherscript.bat
@@ -8,18 +8,7 @@ mode con cols=89 lines=29
 for /F %%a in ('echo prompt $E ^| cmd') do set "ESC=%%a"
 chcp 65001 > nul
 cls
-
-REM               LOADLOGO
-:LOADLOGO
-echo]
-echo]
-echo %ESC%[95m    ██████╗  ██████╗     ██████╗ ██████╗
-echo     ██╔══██╗██╔════╝    ██╔════╝██╔════╝
-echo     ██║  ██║██║  ███╗   ██║     ██║     
-echo     ██║  ██║██║   ██║   ██║     ██║     
-echo     ██████╔╝╚██████╔╝██╗╚██████╗╚██████╗
-echo     ╚═════╝  ╚═════╝ ╚═╝ ╚═════╝ ╚═════╝
-echo                     Python installer
 ping 192.0.2.2 -n 1 -w 1000 > nul
 echo]
 echo %ESC%[31mFile has no updates, %ESC%[32mlast version downloaded!
+pause
