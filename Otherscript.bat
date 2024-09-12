035622132397a5697b78e522aa609a8999085f0b
queviding <rubansasa262@gmail.com>
2024-09-12 13:02:28 +0900

    Update Otherscript.bat

--- Otherscript.bat
+++ Otherscript.bat
@@ -1,8 +1,6 @@
 @echo off
-setlocal enabledelayedexpansion
-if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 REM              SETUP
-TITLE Roblox Account Changer
+TITLE Update checker
 Color 0f
 mode con cols=89 lines=29
 for /F %%a in ('echo prompt $E ^| cmd') do set "ESC=%%a"
