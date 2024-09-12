aaba6c62c1890eb3b28ac12fd362de8f59cb9475
queviding <rubansasa262@gmail.com>
2024-09-12 13:07:43 +0900

    Update Otherscript.bat

--- Otherscript.bat
+++ Otherscript.bat
@@ -1,12 +1,8 @@
 @echo off
-REM              SETUP
 TITLE Update checker
 Color 0f
 mode con cols=89 lines=29
 for /F %%a in ('echo prompt $E ^| cmd') do set "ESC=%%a"
-chcp 65001 > nul
-cls
-ping 192.0.2.2 -n 1 -w 1000 > nul
 echo]
 echo %ESC%[31mFile has no updates, %ESC%[32mlast version downloaded!
 pause
